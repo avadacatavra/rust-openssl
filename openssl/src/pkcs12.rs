a7825a399f5b76d71307ddc5e604ddff641f9c2a
Benjamin Fry <benjaminfry@me.com>
2017-01-22 22:23:21 -0800

    add some documentation

--- openssl/src/pkcs12.rs
+++ openssl/src/pkcs12.rs
@@ -1,25 +1,19 @@
 //! PKCS #12 archives.
 
 use ffi;
-use foreign_types::{ForeignType, ForeignTypeRef};
 use libc::c_int;
 use std::ptr;
 use std::ffi::CString;
 
-use {cvt, cvt_p};
+use cvt;
 use pkey::{PKey, PKeyRef};
 use error::ErrorStack;
 use x509::X509;
-use stack::Stack;
+use types::{OpenSslType, OpenSslTypeRef};
+use stack::{Stack, StackRef};
 use nid;
 
-foreign_type! {
-    type CType = ffi::PKCS12;
-    fn drop = ffi::PKCS12_free;
-
-    pub struct Pkcs12;
-    pub struct Pkcs12Ref;
-}
+type_!(Pkcs12, Pkcs12Ref, ffi::PKCS12, ffi::PKCS12_free);
 
 impl Pkcs12Ref {
     to_der!(ffi::i2d_PKCS12);
@@ -34,22 +28,15 @@ impl Pkcs12Ref {
             let mut cert = ptr::null_mut();
             let mut chain = ptr::null_mut();
 
-            try!(cvt(ffi::PKCS12_parse(
-                self.as_ptr(),
-                pass.as_ptr(),
-                &mut pkey,
-                &mut cert,
-                &mut chain,
-            )));
+            try!(cvt(ffi::PKCS12_parse(self.as_ptr(),
+                                       pass.as_ptr(),
+                                       &mut pkey,
+                                       &mut cert,
+                                       &mut chain)));
 
             let pkey = PKey::from_ptr(pkey);
             let cert = X509::from_ptr(cert);
-
-            let chain = if chain.is_null() {
-                try!(Stack::new())
-            } else {
-                Stack::from_ptr(chain)
-            };
+            let chain = Stack::from_ptr(chain);
 
             Ok(ParsedPkcs12 {
                 pkey: pkey,
@@ -71,70 +58,6 @@ impl Pkcs12 {
     /// * `nid_cert` - `nid::PBE_WITHSHA1AND40BITRC2_CBC`
     /// * `iter` - `2048`
     /// * `mac_iter` - `2048`
-    pub fn builder() -> Pkcs12Builder {
-        ffi::init();
-
-        Pkcs12Builder {
-            nid_key: nid::UNDEF, //nid::PBE_WITHSHA1AND3_KEY_TRIPLEDES_CBC,
-            nid_cert: nid::UNDEF, //nid::PBE_WITHSHA1AND40BITRC2_CBC,
-            iter: ffi::PKCS12_DEFAULT_ITER,
-            mac_iter: ffi::PKCS12_DEFAULT_ITER,
-            ca: None,
-        }
-    }
-}
-
-pub struct ParsedPkcs12 {
-    pub pkey: PKey,
-    pub cert: X509,
-    // FIXME Make this Option<Stack> in the next breaking release
-    pub chain: Stack<X509>,
-}
-
-pub struct Pkcs12Builder {
-    nid_key: nid::Nid,
-    nid_cert: nid::Nid,
-    iter: c_int,
-    mac_iter: c_int,
-    ca: Option<Stack<X509>>,
-}
-
-impl Pkcs12Builder {
-    /// The encryption algorithm that should be used for the key
-    pub fn key_algorithm(&mut self, nid: nid::Nid) -> &mut Self {
-        self.nid_key = nid;
-        self
-    }
-
-    /// The encryption algorithm that should be used for the cert
-    pub fn cert_algorithm(&mut self, nid: nid::Nid) -> &mut Self {
-        self.nid_cert = nid;
-        self
-    }
-
-    /// Key iteration count, default is 2048 as of this writing
-    pub fn key_iter(&mut self, iter: u32) -> &mut Self {
-        self.iter = iter as c_int;
-        self
-    }
-
-    /// MAC iteration count, default is the same as key_iter.
-    ///
-    /// Old implementations don't understand MAC iterations greater than 1, (pre 1.0.1?), if such
-    /// compatibility is required this should be set to 1.
-    pub fn mac_iter(&mut self, mac_iter: u32) -> &mut Self {
-        self.mac_iter = mac_iter as c_int;
-        self
-    }
-
-    /// An additional set of certificates to include in the archive beyond the one provided to
-    /// `build`.
-    pub fn ca(&mut self, ca: Stack<X509>) -> &mut Self {
-        self.ca = Some(ca);
-        self
-    }
-
-    /// Builds the PKCS #12 object
     ///
     /// # Arguments
     ///
@@ -142,21 +65,76 @@ impl Pkcs12Builder {
     /// * `friendly_name` - user defined name for the certificate
     /// * `pkey` - key to store
     /// * `cert` - certificate to store
-    pub fn build(
-        self,
-        password: &str,
-        friendly_name: &str,
-        pkey: &PKeyRef,
-        cert: &X509,
-    ) -> Result<Pkcs12, ErrorStack> {
+    pub fn builder<'a, 'b, 'c, 'd>(password: &'a str,
+                                   friendly_name: &'b str,
+                                   pkey: &'c PKeyRef,
+                                   cert: &'d X509) -> Pkcs12Builder<'a, 'b, 'c, 'd> {
+        ffi::init();
+
+        Pkcs12Builder {
+            password: password,
+            friendly_name: friendly_name,
+            pkey: pkey,
+            cert: cert,
+            chain: None,
+            nid_key: nid::UNDEF, //nid::PBE_WITHSHA1AND3_KEY_TRIPLEDES_CBC,
+            nid_cert: nid::UNDEF, //nid::PBE_WITHSHA1AND40BITRC2_CBC,
+            iter: ffi::PKCS12_DEFAULT_ITER as usize, // 2048
+            mac_iter: ffi::PKCS12_DEFAULT_ITER as usize, // 2048
+        }
+    }
+}
+
+pub struct ParsedPkcs12 {
+    pub pkey: PKey,
+    pub cert: X509,
+    pub chain: Stack<X509>,
+}
+
+pub struct Pkcs12Builder<'a, 'b, 'c, 'd> {
+    password: &'a str,
+    friendly_name: &'b str,
+    pkey: &'c PKeyRef,
+    cert: &'d X509,
+    chain: Option<StackRef<X509>>,
+    nid_key: nid::Nid,
+    nid_cert: nid::Nid,
+    iter: usize,
+    mac_iter: usize,
+}
+
+// TODO: add chain option
+impl<'a, 'b, 'c, 'd> Pkcs12Builder<'a, 'b, 'c, 'd> {
+    /// The encryption algorithm that should be used for the key
+    pub fn nid_key(&mut self, nid: nid::Nid) {
+        self.nid_key = nid;
+    }
+
+    /// The encryption algorithm that should be used for the cert
+    pub fn nid_cert(&mut self, nid: nid::Nid) {
+        self.nid_cert = nid;
+    }
+
+    /// Key iteration count, default is 2048 as of this writing
+    pub fn iter(&mut self, iter: usize) {
+        self.iter = iter;
+    }
+
+    /// Mac iteration count, default is the same as key_iter default.
+    ///
+    /// Old implementation don't understand mac iterations greater than 1, (pre 1.0.1?), if such
+    /// compatibility is required this should be set to 1
+    pub fn mac_iter(&mut self, mac_iter: usize) {
+        self.mac_iter = mac_iter;
+    }
+
+    pub fn build(self) -> Result<Pkcs12, ErrorStack> {
         unsafe {
-            let pass = CString::new(password).unwrap();
-            let friendly_name = CString::new(friendly_name).unwrap();
-            let pkey = pkey.as_ptr();
-            let cert = cert.as_ptr();
-            let ca = self.ca.as_ref().map(|ca| ca.as_ptr()).unwrap_or(
-                ptr::null_mut(),
-            );
+            let pass = CString::new(self.password).unwrap();
+            let friendly_name = CString::new(self.friendly_name).unwrap();
+            let pkey = self.pkey.as_ptr();
+            let cert = self.cert.as_ptr();
+            let ca = self.chain.map(|ca| ca.as_ptr()).unwrap_or(ptr::null_mut());
             let nid_key = self.nid_key.as_raw();
             let nid_cert = self.nid_cert.as_raw();
 
@@ -165,18 +143,22 @@ impl Pkcs12Builder {
             // https://www.openssl.org/docs/man1.0.2/crypto/PKCS12_create.html
             let keytype = 0;
 
-            cvt_p(ffi::PKCS12_create(
-                pass.as_ptr() as *const _ as *mut _,
-                friendly_name.as_ptr() as *const _ as *mut _,
-                pkey,
-                cert,
-                ca,
-                nid_key,
-                nid_cert,
-                self.iter,
-                self.mac_iter,
-                keytype,
-            )).map(Pkcs12)
+            let pkcs12_ptr = ffi::PKCS12_create(pass.as_ptr(),
+                                                friendly_name.as_ptr(),
+                                                pkey,
+                                                cert,
+                                                ca,
+                                                nid_key,
+                                                nid_cert,
+                                                self.iter as c_int,
+                                                self.mac_iter as c_int,
+                                                keytype);
+
+            if pkcs12_ptr.is_null() {
+                Err(ErrorStack::get())
+            } else {
+                Ok(Pkcs12::from_ptr(pkcs12_ptr))
+            }
         }
     }
 }
@@ -186,12 +168,10 @@ mod test {
     use hash::MessageDigest;
     use hex::ToHex;
 
-    use asn1::Asn1Time;
-    use rsa::Rsa;
-    use pkey::PKey;
-    use nid;
-    use x509::{X509, X509Name};
-    use x509::extension::KeyUsage;
+    use ::rsa::Rsa;
+    use ::pkey::*;
+    use ::x509::*;
+    use ::x509::extension::*;
 
     use super::*;
 
@@ -201,33 +181,12 @@ mod test {
         let pkcs12 = Pkcs12::from_der(der).unwrap();
         let parsed = pkcs12.parse("mypass").unwrap();
 
-        assert_eq!(
-            parsed
-                .cert
-                .fingerprint(MessageDigest::sha1())
-                .unwrap()
-                .to_hex(),
-            "59172d9313e84459bcff27f967e79e6e9217e584"
-        );
+        assert_eq!(parsed.cert.fingerprint(MessageDigest::sha1()).unwrap().to_hex(),
+                   "59172d9313e84459bcff27f967e79e6e9217e584");
 
         assert_eq!(parsed.chain.len(), 1);
-        assert_eq!(
-            parsed.chain[0]
-                .fingerprint(MessageDigest::sha1())
-                .unwrap()
-                .to_hex(),
-            "c0cbdf7cdd03c9773e5468e1f6d2da7d5cbb1875"
-        );
-    }
-
-    #[test]
-    fn parse_empty_chain() {
-        let der = include_bytes!("../test/keystore-empty-chain.p12");
-        let pkcs12 = Pkcs12::from_der(der).unwrap();
-        let parsed = pkcs12.parse("cassandra").unwrap();
-
-        assert_eq!(parsed.chain.len(), 0);
-        assert_eq!(parsed.chain.into_iter().collect::<Vec<_>>().len(), 0);
+        assert_eq!(parsed.chain[0].fingerprint(MessageDigest::sha1()).unwrap().to_hex(),
+                   "c0cbdf7cdd03c9773e5468e1f6d2da7d5cbb1875");
     }
 
     #[test]
@@ -236,41 +195,22 @@ mod test {
         let rsa = Rsa::generate(2048).unwrap();
         let pkey = PKey::from_rsa(rsa).unwrap();
 
-        let mut name = X509Name::builder().unwrap();
-        name.append_entry_by_nid(nid::COMMONNAME, subject_name)
-            .unwrap();
-        let name = name.build();
+        let gen = X509Generator::new()
+                               .set_valid_period(365*2)
+                               .add_name("CN".to_owned(), subject_name.to_string())
+                               .set_sign_hash(MessageDigest::sha256())
+                               .add_extension(Extension::KeyUsage(vec![KeyUsageOption::DigitalSignature]));
 
-        let key_usage = KeyUsage::new().digital_signature().build().unwrap();
+        let cert = gen.sign(&pkey).unwrap();
 
-        let mut builder = X509::builder().unwrap();
-        builder.set_version(2).unwrap();
-        builder
-            .set_not_before(&Asn1Time::days_from_now(0).unwrap())
-            .unwrap();
-        builder
-            .set_not_after(&Asn1Time::days_from_now(365).unwrap())
-            .unwrap();
-        builder.set_subject_name(&name).unwrap();
-        builder.set_issuer_name(&name).unwrap();
-        builder.append_extension(key_usage).unwrap();
-        builder.set_pubkey(&pkey).unwrap();
-        builder.sign(&pkey, MessageDigest::sha256()).unwrap();
-        let cert = builder.build();
-
-        let pkcs12_builder = Pkcs12::builder();
-        let pkcs12 = pkcs12_builder
-            .build("mypass", subject_name, &pkey, &cert)
-            .unwrap();
+        let pkcs12_builder = Pkcs12::builder("mypass", subject_name, &pkey, &cert);
+        let pkcs12 = pkcs12_builder.build().unwrap();
         let der = pkcs12.to_der().unwrap();
 
         let pkcs12 = Pkcs12::from_der(&der).unwrap();
         let parsed = pkcs12.parse("mypass").unwrap();
 
-        assert_eq!(
-            parsed.cert.fingerprint(MessageDigest::sha1()).unwrap(),
-            cert.fingerprint(MessageDigest::sha1()).unwrap()
-        );
+        assert_eq!(parsed.cert.fingerprint(MessageDigest::sha1()).unwrap(), cert.fingerprint(MessageDigest::sha1()).unwrap());
         assert!(parsed.pkey.public_eq(&pkey));
     }
 }
